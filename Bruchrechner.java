dab83fcf9f62d863b7f3912a08766cc3dbc5e33e
DESKTOP-UDCGP3J\ENTW1 <jonasofficial015@gmail.com>
2025-03-05 12:18:25 +0100

    Main Methode implementiert

--- Bruchrechner.java
+++ Bruchrechner.java
@@ -9,4 +9,20 @@ public class Bruchrechner {
     private JLabel equalsymbol;
     private JTextField zaehlererg;
     private JTextField nennererg;
+    private JPanel mainpanel;
+    private JLabel filler;
+    private JTextArea ______________________TextArea;
+    private JTextArea ______________________________TextArea1;
+    private JTextArea ______________________________TextArea;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Bruchrechner Binder - BB");
+        frame.setContentPane(new Bruchrechner().mainpanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null); // Fenster zentrieren
+        frame.setVisible(true);
+    }
 }
+
+
