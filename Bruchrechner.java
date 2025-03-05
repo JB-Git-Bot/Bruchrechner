1217de525dd00f9b5ba0a4306e3341d53e8d4147
DESKTOP-UDCGP3J\ENTW1 <jonasofficial015@gmail.com>
2025-03-05 11:51:09 +0100

    Design für Bruchrechner erstellt

---
+++ Bruchrechner.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Bruchrechner {
+    private JTextField zaehler1;
+    private JTextField zaehler2;
+    private JTextField nenner1;
+    private JTextField nenner2;
+    private JComboBox comboBox1;
+    private JLabel equalsymbol;
+    private JTextField zaehlererg;
+    private JTextField nennererg;
+}
